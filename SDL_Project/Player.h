584791eb6d856e5cd39f786573722b9376866084
BIKOMORI <onetwofor15@gmail.com>
2025-12-11 22:34:08 -0500

    20 weapon force fix

--- SDL_Project/Player.h
+++ SDL_Project/Player.h
@@ -25,7 +25,7 @@ public:
     float width = 1.0f;
     float height = 1.8f;
     bool facingRight = true;
-    float meleeDamage = 80.0f;
+    float meleeDamage = 20.0f;
     float meleeRadius = 2.0f;
     bool meleeMode = true;     
     float meleeRange = 1.5f;  
